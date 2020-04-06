63a0b5a9d9b5adca691081f7e72317e0394ccae6
Sasha <ponomarek91@gmail.com>
2020-04-07 00:00:07 +0300

    Updates

---
+++ precache-manifest.767ecd9add17201041208e5bd8dce882.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337ea4ed79c0329775c7ae8818429581",
+    "url": "/Cursor18_HW/index.html"
+  },
+  {
+    "revision": "b2504ca0dea7a52e21f6",
+    "url": "/Cursor18_HW/static/css/main.352c90a9.chunk.css"
+  },
+  {
+    "revision": "40dc31acb1df086e435f",
+    "url": "/Cursor18_HW/static/js/2.78494c07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Cursor18_HW/static/js/2.78494c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2504ca0dea7a52e21f6",
+    "url": "/Cursor18_HW/static/js/main.10dcc893.chunk.js"
+  },
+  {
+    "revision": "3af4b4515789ea7895a0",
+    "url": "/Cursor18_HW/static/js/runtime-main.861cd727.js"
+  },
+  {
+    "revision": "72c941bdd563640381785f90b114a0f5",
+    "url": "/Cursor18_HW/static/media/01e81a35e0d119aed42d2fb15ae6d7e5.72c941bd.jpg"
+  },
+  {
+    "revision": "ef3a41855946f9e8f9411d21a990ecb2",
+    "url": "/Cursor18_HW/static/media/32_1920_1200_beautypic.ru.ef3a4185.jpg"
+  },
+  {
+    "revision": "783bc1bdaadb65c0e8d4d45204f8b42c",
+    "url": "/Cursor18_HW/static/media/prikolnye-kartinki-pro-leto-na-rabochij-stol-1.783bc1bd.jpg"
+  },
+  {
+    "revision": "88c0f6eeb25f75c7adddb9dee335b0a4",
+    "url": "/Cursor18_HW/static/media/unnamed.88c0f6ee.jpg"
+  }
+]);
